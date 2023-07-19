858a18e46d4172f9b9e7711864e5dd415fbdbf9a
Nithish9345 <nithishrajprof09@gmail.com>
2023-07-19 07:34:46 +0530

    .

---
+++ main.py
@@ -0,0 +1,13 @@
+def check_bit(a,b):
+    if a&(1<<b)!=0:
+        return True
+    else:
+        return False
+def unset(a,b):
+    if check_bit(a,b):
+        return a^(1<<b)
+    return a
+
+a =int(input())
+b =int(input())
+print(unset(a, b))
